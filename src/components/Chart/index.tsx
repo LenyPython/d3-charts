fa9abb6eb9ba70cec765e48ba6654ea4388a3009
LenyPython <buniadyserv@gmail.com>
2022-01-06 16:14:18 +0100

    Added mock data for the chart, adding d3 library to requirements. First rect added to svgs

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,14 +1,24 @@
 import './chart.css'
-import {useRef} from "react"
+import * as d3 from 'd3'
+import {useEffect, useRef} from "react"
 import {PriceData} from '../../types'
 
 const Chart: React.FC<{
   data?: PriceData[]
   dimensions?: {[key: string]: string }
 }> = ({data, dimensions}) => {
-  const chartRef = useRef<SVGSVGElement | null>(null)
+  const chartRef = useRef<SVGSVGElement>(null!)
   const svgWidth = dimensions? dimensions.width: '100%'
   const svgHeight = dimensions? dimensions.height: '100%'
+  useEffect(()=>{
+    const SVG = d3.select(chartRef.current)
+    SVG.append('rect')
+      .attr('width', 100)
+      .attr('height', 25)
+      .attr('x', 10)
+      .attr('y', 10)
+
+  })
   if(data) console.log(data)
   return (
     <div className="chart">
