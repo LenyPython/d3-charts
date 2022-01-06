d72371e8e5c0e4697ffa7751210c6fca0b7732c8
LenyPython <buniadyserv@gmail.com>
2022-01-06 21:48:54 +0100

    Trying to draw candlestick

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -20,13 +20,14 @@ const Chart: React.FC<{
     const SvgHeight = chartRef.current.clientHeight
     const SvgWidth= chartRef.current.clientWidth
     //yScale and Axis
-    const maxPrice = d3.max<PriceData, number>(data, d => d.High + 1)!
+    const maxPrice = d3.max<PriceData, number>(data, d => d.High + 0.3)!
+    const minPrice = d3.max<PriceData, number>(data, d => d.Low - 0.3)!
     //get height and width directly from svg
     const yScaleHeight = SvgHeight - 50 - MARGIN
     const yScalePosX = SvgWidth - 50
     const yScalePosY = MARGIN
     const yScale = d3.scaleLinear()
-          .domain([0, maxPrice])
+          .domain([minPrice, maxPrice])
           .range([yScaleHeight, MARGIN]  )
     const yAxis = d3.axisRight(yScale)
     //append y axis to svg chart
@@ -37,23 +38,24 @@ const Chart: React.FC<{
     const xScalePosY = SvgHeight - 50
     const xScalePosX = MARGIN
     const xScale = d3.scaleBand()
-    .domain(data.map(d=>d.Date))
+        .domain(data.map(d=>d.Date))
         .range([MARGIN, SvgWidth - 50 - MARGIN])
-    .padding(0.2)
+        .padding(0.2)
     const xAxis = d3.axisBottom(xScale)
     //append x axis to svg chart
     SVG.append('g')
     .attr('transform',`translate(${xScalePosX},${xScalePosY})`)
     .call(xAxis)
     //draw data candlesticks
+    //draw candle body 'rect'
     SVG.selectAll('rect')
       .data(data)
       .enter()
       .append('rect')
       .attr('x', d => xScale(d.Date)!)
-      .attr('y', d => yScale(d.Open)!)
-      .attr('width', 55)
-      .attr('height', d => yScale(d.Close)!)
+      .attr('y', d => d.Open > d.Close ? yScale(d.Open)!: yScale(d.Close)!)
+      .attr('width', xScale.bandwidth())
+      .attr('height', d => Math.abs(yScale(d.Close)! - yScale(d.Open)!))
       .attr('fill', d => d.Open > d.Close? 'red':'blue')
 
   },[data])
--- src/components/Chart/mock.ts
+++ src/components/Chart/mock.ts
@@ -3,17 +3,17 @@
 export const DATA = [
 	{
 		Date:'12/30/21',
-		Open:1.1352,
-		High:1.1361,
+		Open:1,
+		High:2.1361,
 		Low:1.1299,
-		Close:1.1327
+		Close:2
 	},
 	{
 		Date:'12/31/21',
-		Open:1.1324,
-		High:1.1387,
-		Low:1.1303,
-		Close:1.1373
+		Open:2,
+		High:3.5,
+		Low:0.2,
+		Close:1.5
 	},
 {
 		Date:'01/03/22',
