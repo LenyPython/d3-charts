cff70d04b17f02d362baff201b7d4bc92e3c9996
LenyPython <buniadyserv@gmail.com>
2022-01-10 19:08:48 +0100

    Changed bottom bar, adding console to it

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,12 +1,19 @@
 import {useState} from 'react'
+import {useAppSelector} from '../../app/hooks'
+import {getLogs} from '../../slices/WebSocket'
 import './transactions.css'
 
 const Transactions = () => {
+  const LOGS = useAppSelector(getLogs)
   const [isOpen, setIsOpen] = useState(true)
   return (
-    <div id="Transactions" onClick={()=>setIsOpen(!isOpen)}className={isOpen?'open':'closed'}>
-      <h1>helloword</h1>
-      <h1>helloword</h1>
+    <div id="lower-panel" onClick={()=>setIsOpen(!isOpen)}className={isOpen?'open':'closed'}>
+      <div id='logger'>
+        {
+          LOGS.map((item, id) =>  <div key={id}>{item}</div>)
+        }
+      </div>
+      <div id="transactions"></div>
     </div>
   )
 
