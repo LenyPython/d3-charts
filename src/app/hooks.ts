44893e73965ee46299d9ed04d491fe5082350368
LenyPython <buniadyserv@gmail.com>
2022-01-10 18:14:42 +0100

    Creating a websocket connect hook. Creating basic types for commands to be created

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,7 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import {PriceData} from '../types';
 import type { RootState, AppDispatch } from './store';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {addLog} from '../slices/WebSocket'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
@@ -12,3 +13,29 @@ export const usePriceData = (data: PriceData[]) => useState<PriceData[]>(data)
 
 export type priceDataValue = ReturnType<typeof usePriceData>[0]
 export type priceDataSet = ReturnType<typeof usePriceData>[1]
+
+
+//websocket hook
+export const useWebsocket = (url: string) => {
+  const dispatch = useAppDispatch()
+  const onOpen = (event: any) =>{
+    dispatch(addLog('Connected to the server!'))
+  }
+  const onClose = (event: any) =>{ dispatch(addLog('! Connection Closed !')) }
+  const onError = (event: any) =>{ dispatch(addLog(JSON.stringify(event.data))) }
+  const onMessage = (event: any) => {
+    switch(event.command){
+      default:
+        console.log(JSON.stringify(event.data))
+    }
+  }
+  useEffect(()=> {
+    const WS = new WebSocket(url)
+    WS.onopen = onOpen
+    WS.onerror = onError
+    WS.onmessage = onMessage
+    WS.onclose = onClose
+
+
+  }, [url])
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,18 @@
-
-
 export interface PriceData {
 	Date: string
 	Open: number
 	Close: number
 	High: number
 	Low: number
+}
+
+type COMMAND = "login"
+type LoginCredentials = {
+		userId: string
+		password: string
+		appName?: string
+}
+export interface JSONLogin {
+	command: COMMAND
+	arguments: LoginCredentials
 }
