d99ac52df509899bed6f717d7e43688e5dddfe46
LenyPython <buniadyserv@gmail.com>
2022-01-11 15:01:06 +0100

    Working with some simplestyles

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,4 +1,6 @@
+import './LoginForm.css'
 import {Dispatch, SetStateAction, useState} from 'react'
+
 
 const LoginForm:React.FC<{
   setIsOpen: Dispatch<SetStateAction<boolean>>
@@ -11,10 +13,24 @@ const LoginForm:React.FC<{
   }
   
   return(
-    <div id='login-form'>
-    <form>
-      <input type="name" value={user} onChange={e=>setUser(e.target.value)} required/>
-      <input type="password" value={password} onChange={e=>setPassword(e.target.value)}required/>
+    <div id='login-form' className='flex-center'>
+    <form className='flex-center'>
+      <input 
+      type="name"
+      value={user}
+      onChange={e=>setUser(e.target.value)}
+      placeholder="Userlogin"
+      required
+      />
+      <br />
+      <input 
+      type="password"
+      value={password}
+      onChange={e=>setPassword(e.target.value)}
+      placeholder="Password"
+      required
+      />
+      <br />
       <button onClick={login}>Login</button>
     </form>
       <button  className="close-btn" onClick={()=>setIsOpen(false)}>Close</button>
