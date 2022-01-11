add80c1341b010fb4a00cd826e77ab9c0fd121e0
LenyPython <buniadyserv@gmail.com>
2022-01-11 14:35:36 +0100

    Basic loading indexes

--- src/components/Indexes/index.tsx
+++ src/components/Indexes/index.tsx
@@ -1,14 +1,15 @@
 import {useState} from 'react'
+import {useAppSelector} from '../../app/hooks'
+import {getIndexes} from '../../slices/Indexes'
 import './indexes.css'
 
 const Indexes = () => {
   const [isOpen, setIsOpen] = useState(true)
+  const indexes = useAppSelector(getIndexes)
 
   return (
       <div id="indexes" className={isOpen?'open':'closed'}>
-        <p>a</p>
-        <p>b</p>
-        <p>c</p>
+        {indexes.map((item: string)=><div key={item}>{item}</div>)}
       </div>
   )
 }
