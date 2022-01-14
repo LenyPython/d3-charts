e9508248899496c54662380f5e2fb7f73fd4555a
LenyPython <buniadyserv@gmail.com>
2022-01-14 19:40:26 +0100

    Changes to on eventsaga channel. Now i have onmessage websocket checker

--- src/sagas/WebsocketSaga.ts
+++ src/sagas/WebsocketSaga.ts
@@ -1,13 +1,19 @@
 import { takeLeading, fork, take, put, call, Effect } from 'redux-saga/effects'
-import {addLog} from '../slices/WebSocket'
+import {addLog, setSessionId} from '../slices/WebSocket'
 import { WSACTIONS } from './types'
-import {LoginCredentials} from '../types'
+import {COMMAND, HashedInstruments, instrumentCategory,
+	LoginCredentials} from '../types'
 import {send} from '../utils/websocket'
 import {Disconnect, LoginCommand} from '../app/commands'
+import {WebSocketConnect} from './actions'
 import {eventChannel} from 'redux-saga'
+import {setIndexes} from '../slices/Indexes'
 
 
 let WS: WebSocket | null = null
+let userId = ''
+let password = ''
+const appName = process.env.REACT_APP_APP_NAME
 
 export default function* WebSocketSaga() {
 	yield takeLeading(WSACTIONS.connect, WebSocketConnectWorker)
@@ -15,14 +21,16 @@ export default function* WebSocketSaga() {
 }
 
 function* WebSocketConnectWorker(action: Effect<WSACTIONS, LoginCredentials>){
-	const {userId, password} = action.payload
+	const {userId: id, password: pass} = action.payload
+	userId = id
+	password = pass
 	const URL = process.env.REACT_APP_SOCKET_URL
-	const appName = process.env.REACT_APP_APP_NAME
 	yield put(addLog('[Login] Trying to log in...'))
 	try{
 		if(!URL) throw new Error('You forgot to declare REACT_APP_SOCKET_URL')
 		if(WS === null) WS = new WebSocket(URL)
-		const eventWatcherTask = yield fork(WebSocketEventWatcher)
+		//create separate websocket event channel/listener
+		yield fork(WebSocketEventWatcher)
 			WS.onopen = () => {
 				//create login command/credentials and send login request
 				const msg = new LoginCommand(userId, password, appName)
@@ -36,6 +44,7 @@ function* WebSocketConnectWorker(action: Effect<WSACTIONS, LoginCredentials>){
 
 function* WebSocketDisconnectWorker(){
 	yield call(send, WS!, new Disconnect())
+	yield put(setSessionId(null))
 }
 
 const createWebSocketChannel = (socket: WebSocket) => {
@@ -47,10 +56,52 @@ const createWebSocketChannel = (socket: WebSocket) => {
 		const closeHandler = (e: CloseEvent) => {
 			console.log('close', e)
 			emit(addLog(`[Socket Close]: ${e.reason ? e.reason : `code: ${e.code}`}`))
+			if(userId && password) {
+				setTimeout(()=> {
+					emit(WebSocketConnect({userId, password, appName}))
+				}, 3500)
+			}
 		}
+		const handleMessages = (
+			emit:(input: unknown) => void,
+			response: any) => {
+		if(response.status === false) emit(addLog('[Error]: response failed'))
+		else if(response.status === true){
+			if(response.streamSessionId) {
+				emit(setSessionId(response.streamSessionId))
+				send(WS!, { command: COMMAND.getAllSymbols })
+			}
+			else {
+				const data: instrumentCategory[] = response.returnData
+				let instruments = {} as HashedInstruments
+				for(let instrument of data){
+					const {categoryName, groupName, swapLong, swapShort, symbol} = instrument
+					if(instruments[categoryName] === undefined)	instruments[categoryName] = {}
+					instruments[categoryName][groupName] = {
+						symbol,
+						swapShort,
+						swapLong
+					}
+				}
+			emit(setIndexes(instruments))
+			}
+		}
+		else {
+			emit(addLog('otherevent'))
+		}
+	}
 
 		socket.onerror = errorHandler
 		socket.onclose = closeHandler
+		socket.onmessage = (event: MessageEvent<any>) => {
+			try{
+				const response = JSON.parse(event.data)
+				handleMessages(emit, response)
+
+			} catch(e) {
+				if(e instanceof Error) emit(addLog(`[Error onmsg]: ${e.message}`))
+			}
+		}
 
 		const unsubscribe = () => {
 			socket.close()
