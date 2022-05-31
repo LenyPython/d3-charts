74f957058eac5629189dfb15b4026d3b530489ab
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2022-06-01 00:22:53 +0200

    d3fc resizing charts

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,5 +1,6 @@
-import { min, max, scaleLinear, scaleTime } from 'd3'
-import { useRef } from 'react'
+import { select, scaleLinear, scaleTime } from 'd3'
+import * as fc from 'd3fc'
+import { useEffect, useRef } from 'react'
 import { PriceData } from '../../types'
 import { createData } from '../../utils/mock'
 import './chart.css'
@@ -11,19 +12,37 @@ const HEIGHT = 600
 const Chart: React.FC<{
   data: PriceData[]
 }> = ({ data }) => {
-  const svgRef = useRef<SVGSVGElement>(null!)
+  const svgRef = useRef<HTMLDivElement>(null!)
 
   if (process.env.REACT_DEBUG === 'true') data = createData(100)
-  if (!data) return <></>
+  if (!data) data = []
+
   const xScale = scaleTime().range([MARGIN.LEFT, WIDTH - MARGIN.RIGHT])
   const yScale = scaleLinear().range([HEIGHT - MARGIN.BOTTOM, MARGIN.TOP])
-  return (
-    <div className="container">
-      <svg ref={svgRef} viewBox={`0 0 100% 100%`}>
-        <rect width={'100%'} height={'100%'} fill="white" />
-      </svg>
-    </div>
-  )
+
+  const yExtent = fc.extentLinear().accessors([(d: PriceData) => d.high, (d: PriceData) => d.low])
+  const xExtent = fc.extentTime().accessors([(d: PriceData) => d.ctm])
+
+  const candlestick = fc
+    .seriesSvgCandlestick()
+    .xScale(xScale)
+    .yScale(yScale)
+    .highValue((d: PriceData) => d.high)
+    .lowValue((d: PriceData) => d.low)
+    .openValue((d: PriceData) => d.open)
+    .closeValue((d: PriceData) => d.close)
+
+  const multi = fc.seriesSvgMulti().series([candlestick])
+
+  const chart = fc.chartCartesian(xScale, yScale).svgPlotArea(multi)
+
+  chart.xDomain(xExtent(data))
+  chart.yDomain(yExtent(data))
+
+  useEffect(() => {
+    select(svgRef.current).datum(data).call(chart)
+  }, [data, chart])
+  return <div ref={svgRef}></div>
 }
 
 export default Chart
