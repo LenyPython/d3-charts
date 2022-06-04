ce147002131d3f35978a1f04192c6589bee29857
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2022-06-04 23:57:54 +0200

    xScale simple zoom

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,6 +1,6 @@
-import { select, scaleLinear, scaleBand, nice } from 'd3'
+import { select, scaleLinear, scaleBand } from 'd3'
 import * as fc from 'd3fc'
-import { useEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import { PriceData } from '../../types'
 import { createData } from '../../utils/mock'
 import './chart.css'
@@ -9,15 +9,20 @@ const Chart: React.FC<{
   data: PriceData[]
 }> = ({ data }) => {
   const svgRef = useRef<HTMLDivElement>(null!)
+  const length = data.length
+  const [zoomIndex, setZoomIndex] = useState<number>(0)
 
   if (process.env.REACT_DEBUG === 'true') data = createData(100)
   if (!data) data = []
+  else data = data.slice(zoomIndex)
 
   const yExtent = fc.extentLinear().accessors([(d: PriceData) => d.high, (d: PriceData) => d.low])
   // const xExtent = fc.extentTime().accessors([(d: PriceData) => d.ctmString])
 
-  const yScale = scaleLinear()
+  const yScale = scaleLinear().domain(yExtent(data))
   const xScale = scaleBand()
+    .domain(data.map((d) => d.ctmString))
+    .paddingOuter(5)
 
   const candlestick = fc
     .autoBandwidth(fc.seriesSvgCandlestick())
@@ -29,17 +34,17 @@ const Chart: React.FC<{
 
   const multi = fc.seriesSvgMulti().series([candlestick])
 
-  const chart = fc
-    .chartCartesian({ xScale, yScale })
-    .svgPlotArea(multi)
-    .yDomain(yExtent(data))
-    .xDomain(data.map((d) => d.ctmString))
-    .yNice()
+  const chart = fc.chartCartesian({ xScale, yScale }).svgPlotArea(multi).xTickValues([]).yNice()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     select(svgRef.current).datum(data).call(chart)
   }, [data, chart])
-  return <div ref={svgRef}></div>
+  const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+    if (e.deltaY > 0 && zoomIndex >= 15) setZoomIndex((idx: number) => idx - 10)
+    else if (e.deltaY > 0) setZoomIndex(0)
+    else if (zoomIndex < length - 30) setZoomIndex((idx: number) => idx + 10)
+  }
+  return <div onWheel={handleWheel} ref={svgRef}></div>
 }
 
 export default Chart
