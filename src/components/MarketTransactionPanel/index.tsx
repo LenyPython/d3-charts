4858767e9242d752e5056c115fccbf72cb6c9807
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2022-06-25 20:34:34 +0200

    Better market buy profile, added market depth

--- src/components/MarketTransactionPanel/index.tsx
+++ src/components/MarketTransactionPanel/index.tsx
@@ -8,6 +8,7 @@ import { getInstrumentCurrentPrice } from '../../store/OpenedInstrumentsStream/s
 
 const MarketTransactionPanel = () => {
   const dispatch = useAppDispatch()
+  const [volume, setVolume] = useState(0.01)
   const symbol = useAppSelector(getCurrentChartSymbol)
   const prices = useAppSelector(getInstrumentCurrentPrice)[symbol]
   const asks = [] as JSX.Element[]
@@ -15,31 +16,23 @@ const MarketTransactionPanel = () => {
   for (let level in prices) {
     const item = prices[level]
     asks.push(
-      <div key={`market-ask-${level}`}>
-        <button className="btn sell" onClick={() => marketOpenOrderHandler(CMD.SELL, item.ask)}>
-          {item.ask}
-        </button>
-        {item.askVolume}
+      <div key={`market-ask-${level}`} className="depth-container df jcsa">
+        <div>{item.ask}</div> <div>{item.askVolume}</div>
       </div>,
     )
     buys.push(
-      <div key={`market-bid-${level}`}>
-        <button className="btn buy" onClick={() => marketOpenOrderHandler(CMD.BUY, item.bid)}>
-          {item.bid}
-        </button>
-        {item.bidVolume}
+      <div key={`market-bid-${level}`} className="depth-container df jcsa">
+        <div>{item.bid}</div> <div>{item.bidVolume}</div>
       </div>,
     )
   }
-  const [volume, setVolume] = useState(0.01)
-  const marketOpenOrderHandler = (cmd: CMD, price: number) => {
+  const marketOpenOrderHandler = (cmd: CMD) => {
     dispatch(
       sendMarketOrderRequest({
         cmd,
         type: TYPE.OPEN,
         symbol,
         volume,
-        price,
       }),
     )
   }
@@ -48,8 +41,14 @@ const MarketTransactionPanel = () => {
     setVolume(parseFloat(target.value))
   }
   return (
-    <div id="transaction-panel-container">
+    <div id="transaction-panel-container" className="dfc aic">
       <h5>Market Transaction</h5>
+      <div>Daily High: {prices['level0'].high}</div>
+      {asks.reverse()}
+      {prices['level0'].askVolume}
+      <button className="btn sell" onClick={() => marketOpenOrderHandler(CMD.SELL)}>
+        {prices['level0'].ask}
+      </button>
       <input
         type="number"
         onChange={handleVolumeChange}
@@ -58,12 +57,12 @@ const MarketTransactionPanel = () => {
         step="0.01"
         value={volume}
       />
-      <div>{prices['level0'].high}</div>
-      <div>
-        {asks.reverse()}
-        {buys}
-      </div>
-      <div>{prices['level0'].low}</div>
+      <button className="btn buy" onClick={() => marketOpenOrderHandler(CMD.BUY)}>
+        {prices['level0'].bid}
+      </button>
+      {prices['level0'].bidVolume}
+      {buys}
+      <div>Daily Low: {prices['level0'].low}</div>
     </div>
   )
 }
