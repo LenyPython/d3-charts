c3d5a29654c9474aea0663881d59450cc429ecfe
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2022-06-28 21:41:26 +0200

    Basind 5 level depth of market data

--- src/components/MarketDepthChart/index.tsx
+++ src/components/MarketDepthChart/index.tsx
@@ -5,11 +5,13 @@ import { getInstrumentCurrentPrice } from '../../store/OpenedInstrumentsStream/s
 import { TradePriceData } from '../../store/OpenedInstrumentsStream/types'
 import { useResizeObserver } from '../Chart/d3components/hooks'
 import { priceFormat } from '../Chart/d3components/utils'
+import { scaleLinear } from 'd3'
 import './MarketDepthChart.css'
-import createYScale from './utils'
 
 const xOffset = 5
 const marginRight = 50
+const margin = 10
+
 const MarketDepthChart = () => {
   const divRef = useRef<HTMLDivElement>(null)
   const size = useResizeObserver(divRef)
@@ -17,13 +19,22 @@ const MarketDepthChart = () => {
   const height = size?.height ?? 30
   const symbol = useAppSelector(getCurrentChartSymbol)
   const depthOfMarket = useAppSelector(getInstrumentCurrentPrice)[symbol]
-  const yScale = createYScale(depthOfMarket, height)
-  const xLinePos = useMemo(() => width - marginRight, [width])
-  const xTickPos = useMemo(() => xLinePos + xOffset, [xLinePos])
+  const mainChartWidth = useMemo(() => width - marginRight, [width])
+  const xTickPos = useMemo(() => mainChartWidth + xOffset, [mainChartWidth])
+  if (!depthOfMarket) return <h2>awaiting data</h2>
+  const yScale = scaleLinear()
+    .domain([depthOfMarket[4].low, depthOfMarket[4].high])
+    .range([margin, height - margin])
   return (
     <div id="market-depth-container" ref={divRef}>
       <svg viewBox={`0 0 ${width} ${height}`}>
-        <line x1={xLinePos} x2={xLinePos} y1={0} y2={'100%'} strokeWidth={2} />
+        <line
+          x1={mainChartWidth}
+          x2={mainChartWidth}
+          y1={margin}
+          y2={height - margin}
+          style={{ strokeWidth: '2px' }}
+        />
         {yScale.ticks(10).map((tickValue: number) => {
           return (
             <g key={`y-tick-${tickValue}`}>
@@ -33,10 +44,25 @@ const MarketDepthChart = () => {
             </g>
           )
         })}
-        {depthOfMarket.map((item: TradePriceData) => {
+        {depthOfMarket.map((item: TradePriceData, i: number) => {
           return (
             <>
-              <line y1={yScale(item.ask)} y2={yScale(item.ask)} x1={0} x2={30} />
+              <line
+                key={`asker-market-depth-${i}`}
+                className="askers"
+                y1={yScale(item.ask)}
+                y2={yScale(item.ask)}
+                x1={0}
+                x2={(mainChartWidth * item.askVolume) / 10000000}
+              />
+              <line
+                key={`biders-market-depth-${i}`}
+                className="biders"
+                y1={yScale(item.bid)}
+                y2={yScale(item.bid)}
+                x1={0}
+                x2={(mainChartWidth * item.bidVolume) / 10000000}
+              />
             </>
           )
         })}
